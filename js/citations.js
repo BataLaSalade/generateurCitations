df917b1d99fe4ff018925293cc690afca199f8a9
BataLaSalade <dsfmarina@gmail.com>
2018-06-14 14:37:53 +0200

    Ajout des consignes

---
+++ js/citations.js
@@ -0,0 +1,8 @@
+/*
+----- LES CONSIGNES -----
+Créer un générateur de citations
+    Les citations sont générées aléatoirement une par une à chaque appel du programme JavaScript
+    Chaque citation est la combinaison d'au moins 3 morceaux de phrases (à preparer soit-même)
+    A défaut d'être intélligible, la phrase doit être cohérente (pas de point d'exclamation au milieu d'une phrase)
+Le programme peut afficher le résultat dans la console JavaScript
+*/
